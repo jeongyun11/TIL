209359f76d4bbae863e9ec2dcb6beafca3c6f7b9
jeongyun11 <uwn1004@gmail.com>
2023-02-22 01:18:30 +0900

    Add 230221.md

--- TIL(~230204)/KDT_2nd/DB/230215_practice.sql
+++ TIL(~230204)/KDT_2nd/DB/230215_practice.sql
@@ -8,6 +8,8 @@ INNER JOIN offices
 ON employees.officeCode = offices.officeCode
 ORDER BY employeeNumber;
 
+SELECT * FROM employees;
+SELECT * FROM offices;
 -- 2 # '사무실이 없는 직원을 포함한' 모든 직원을 출력한다.
 SELECT customerNumber ,officeCode, customers.city, offices.city
 FROM customers
@@ -29,10 +31,15 @@ INNER JOIN offices
 ON customers.city = offices.city
 ORDER BY customerNumber DESC;
 
--- 5 ∩∪
-# 2. customers '사무실이 없는 직원을 포함한' 모든 직원.
-# 3. offices '직원이 없는 사무실을 포함한' 모든 사무실.
-# 4. (customers ∩ offices) 사무실이 있는 직원 or 직원이 있는 사무실
+
+SELECT * FROM customers;
+SELECT * FROM offices;
+저기서 customers와 offices를 city로 연결했다는 뜻은 customers.city는 '고객사무실의 위치' 로 봤습니다
+만약 customers.city가 london이여도 '정확한 사무실의 주소'가 offices.city가 london인 사무실과 다르면 잘못된 결과가 나오는 거라고 생각합니다.
+-- 5 # 
+# 2. customers 'offices사무실에 안 다니는 직원을 포함한' 모든 직원. 
+# 3. offices 'customers고객이 안 다니는 사무실을 포함한' 모든 사무실.
+# 4. (customers ∩ offices) customers고객중 offices사무실에만 다니는 고객 or offices사무실중 customers고객만 다니는 사무실
 
 -- 6
 SELECT customerNumber, officeCode, customers.city, offices.city
@@ -69,8 +76,8 @@ INNER JOIN orders
 ON orderdetails.orderNumber = orders.orderNumber
 INNER JOIN products
 ON orderdetails.productCode = products.productCode
-ORDER BY orderNumber;
-
+ORDER BY orderNumber
+W
 -- 
 SELECT * FROM orders;
 SELECT * FROM orderdetails;
